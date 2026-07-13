0560f514b99e98f0a08acc7c8643a74360c173c1
Harshika Anand <hanand_be23@thapar.edu>
2026-07-13 19:31:17 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            int p=1;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i!=j)
+                p*=nums[j];
+            }
+            ans[i]=p;
+        }
+        return ans;
+    }
+};
